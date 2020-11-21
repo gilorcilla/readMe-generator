4cbdc4d447e080161a2b4ce77abf0fa056ba0130
Gil Orcilla <mathnasiumcos@gmail.com>
2020-11-21 13:52:34 -0700

    provide add'l codes

--- index.js
+++ index.js
@@ -1,27 +1,28 @@
-const inquirer = require("inquirer")
-const fs = require("fs")
+const inquirer = require("inquirer");
+const fs = require("fs");
 const generateMarkDown = require("./utils/generateMarkdown");
 const util = require("util");
 
 const writeFileAsynch = util.promisify(fs.writeFile);
 
-
 // array of questions for user
 const questions = [
-    "What is the title of your project?",
-    "What is your project description"?,
-    
-
+  "What is the title of your project?",
+  "What is your project description?",
+  "What are your installation requirements?",
+  "How do you use this application?",
+  "What license would you like to use?  Please select one.",
+  "Are you open to contributions?",
+  "What command will run your test?",
+  "Enter your GitHub username.",
+  "What is your email address>",
 ];
 
 // function to write README file
-function writeToFile(fileName, data) {
-}
+function writeToFile(fileName, data) {}
 
 // function to initialize program
-function init() {
-
-}
+function init() {}
 
 // function call to initialize program
 init();
