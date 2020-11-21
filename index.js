1226c8a8a2309c99cab34661b75bd6f2fbe30b7e
Gil Orcilla <mathnasiumcos@gmail.com>
2020-11-21 15:01:19 -0700

    modified files and add package

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const inquirer = require("inquirer");
 const fs = require("fs");
 const generateMarkDown = require("./utils/generateMarkdown");
 const util = require("util");
+const { type } = require("os");
 
 const writeFileAsynch = util.promisify(fs.writeFile);
 
@@ -19,10 +20,60 @@ const questions = [
 ];
 
 // function to write README file
-function writeToFile(fileName, data) {}
+//function writeToFile(fileName, data) {}
+const promptUser = () => {
+  return inquirer.prompt([
+    {
+      type: "input",
+      name: "title",
+      message: questions[0],
+    },
+    {
+      type: "input",
+      name: "description",
+      message: questions[1],
+    },
+    {
+      type: "input",
+      name: "installation",
+      message: questions[2],
+    },
+    {
+      type: "input",
+      name: "usage",
+      message: questions[3],
+    },
+    {
+      type: "list",
+      name: "license",
+      message: questions[4],
+      choices: ["BSD 3-Clause", "MIT", "Apache License", "BSD 2-Clause"],
+    },
+    {
+      type: "input",
+      name: "contribution",
+      message: questions[5],
+    },
+    {
+      type: "input",
+      name: "testInstructions",
+      message: questions[6],
+    },
+    {
+      type: "input",
+      name: "gitHubUser",
+      message: questions[7],
+    },
+    {
+      type: "input",
+      name: "email",
+      message: questions[8],
+    },
+  ]);
+};
 
 // function to initialize program
-function init() {}
+asyn function init() {}
 
 // function call to initialize program
 init();
