5c9dc0dcd75ec2f651e96e93ba0e576b498d3b7f
Gil Orcilla <mathnasiumcos@gmail.com>
2020-11-22 11:14:58 -0700

    add more codes to the file

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -2,7 +2,7 @@
 function generateMarkdown(userResponse, userInfo) {
   
   // Generate Table of Contents, based on userResponses
-  let draftToc = `### Table of Contents`;
+  let draftToc = `## Table of Contents`;
 
   if (userResponse.installation !== '') { draftToc += `
   * [Installation] (#installation)` };
@@ -10,7 +10,35 @@ function generateMarkdown(userResponse, userInfo) {
   if (userResponse.usage !== '') { draftToc += `
   * [Usage] (#usage)`};
 
-  if (userResponse.contributing)
+  if (userResponse.contributing !== '') { draftToc += `
+  * [Contributing] (#contributing)`};
+
+  If (userResponse.tests !== '') { draftToc += `
+  * [Tests] (#tests)`};
+
+  // Generate markdown for the upper portion of the README.md file
+  let draftMarkdown = `# ${userResponse.title}
+  
+  ![Badge for GitHub repo top language](https://img.shields.io/github/languages/top/${userResponses.username}/${userResponse.repo}?style=flat&logo=appveyor) ![Badge for GitHub last commit](https://img.shields.io/github/last-commit/${userResponses.username}/${userResponses.repo}?style=flat&logo=appveyor)
+  
+  Check out the badges hosted by [shields.io](https://shields.io).
+  
+  ## Description
+  
+  * The what , why, and how:*
+  
+  ${userResponses.description}
+  
+  `
+
+  // Add Table of Contents (Toc) to markdown
+  draftMarkdown += draftToc;
+
+  // Add License section since it is required to Table of Contents
+  draftMarkdown += ` * [License](#license)`;
+
+  // Optional Installation section
+  if (userResponses.installation !== '') {}
 }
 
 module.exports = generateMarkdown;
